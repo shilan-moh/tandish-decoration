bc26f0c87486c30651d31b42cb26191c02328887
shilan-moh <mohammadi.shilann@gmail.com>
2022-12-21 11:02:19 +0100

    addind css

---
+++ shopping-app/src/App.js
@@ -0,0 +1,8 @@
+import CategoriesData from "./components/categories-data/categories-data.component";
+import "./categories.styles.scss";
+
+const App = () => {
+  return <CategoriesData />;
+};
+
+export default App;
